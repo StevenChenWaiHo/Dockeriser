81bdcfb45f24fbdf69a7f4111dd1c0cf3f6c55f7
Steven Chen <122108964+StevenChenWaiHo@users.noreply.github.com>
2024-08-22 15:53:14 +0100

    Add scripts to dockerise all Node.js Projects

--- dockerise.sh
+++ dockerise.sh
@@ -1,1 +1,78 @@
-echo This is a test to see if this can be downloaded
+#!/bin/bash
+DOCKERFILE_HEADER_STRING=$(cat << EOF
+version: '3'
+
+services:
+EOF
+)
+
+function get_plugin_docker_string () {
+    PLUGIN_STRING=$(cat << EOF
+    build-$FOLDER_NAME:
+      labels:
+        - "traefik.enable=false"
+      image: pugpig/wpdev:v0
+      restart: "no"
+      volumes:
+        - ./$FOLDER_PATH:/$FOLDER_PATH
+      user: app
+      command: bash -c '''
+          cd /$FOLDER_PATH
+          && composer update --no-interaction
+          && composer install --no-interaction
+          && composer dumpautoload --optimize
+          && composer test --no-interaction
+          && npm ci
+          && NO_GIT=1 npm run watch
+        '''
+
+EOF
+)
+}
+
+function append_plugin_docker_string() {
+    # Loop through each repository
+    echo "Processing $FOLDER_NAME..."
+
+    # Navigate to the repository directory
+    cd "$FOLDER_PATH" || { echo "Failed to enter $FOLDER_PATH"; return; }
+
+    # Check if there is a package.json file
+    if [ ! -f "package.json" ]; then
+        echo "$FOLDER_NAME directory is not a valid Node.js project (Cannot find the package.json file)"; 
+        cd ..
+        return;
+    fi
+
+    echo "Detected Node.js project"
+
+    # Check if Dockerfile exists, if not create one
+    if [ -f "docker-compose2.yml" ]; then
+        echo "docker file (docker-compose.yml) already exists try deleting it before running this script"
+    fi
+
+    # Create a basic Dockerfile (you might need to adjust this based on repo needs)
+    get_plugin_docker_string
+    DOCKERFILE_PLUGIN_STRING+="$PLUGIN_STRING\n"
+
+    cd ..
+}
+
+# MAIN STARTS HERE
+
+DOCKERFILE_PLUGIN_STRING=""
+
+NODE_FOLDERS=($(find . -name "package.json" -exec dirname {} \;))
+
+CURRENT_REPO=$(pwd)
+
+echo $NODE_FOLDERS
+
+for FOLDER_PATH in "${NODE_FOLDERS[@]}"; do
+    FOLDER_NAME=$(basename $FOLDER_PATH)
+    append_plugin_docker_string
+    cd "$CURRENT_REPO"
+done
+
+echo "Creating Dockerfile"
+echo -e "$DOCKERFILE_HEADER_STRING\n$DOCKERFILE_PLUGIN_STRING" > docker-compose2.yml
